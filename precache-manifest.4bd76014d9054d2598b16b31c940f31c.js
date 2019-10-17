6067c9738c99ff748303dd5987ee75d4976f5957
MOSES <MOSESLAZARO2019@outlook.com>
2019-10-17 03:04:52 +0300

    Updates

---
+++ precache-manifest.4bd76014d9054d2598b16b31c940f31c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82346d9edd20b399e2d2bed938a28c19",
+    "url": "/City-tour-project/index.html"
+  },
+  {
+    "revision": "cf36c906c069096b11ca",
+    "url": "/City-tour-project/static/css/main.83534a67.chunk.css"
+  },
+  {
+    "revision": "12be3d09016b16625de3",
+    "url": "/City-tour-project/static/js/2.8b6483c7.chunk.js"
+  },
+  {
+    "revision": "cf36c906c069096b11ca",
+    "url": "/City-tour-project/static/js/main.67222713.chunk.js"
+  },
+  {
+    "revision": "9cb78088a6c0ac929faa",
+    "url": "/City-tour-project/static/js/runtime-main.a3bd1fcb.js"
+  }
+]);
